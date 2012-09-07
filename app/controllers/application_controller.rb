cdd9b4595735a8c74d9d9f2800062191a1f2be5e
Lucas Minissale <lucasminissale@gmail.com>
2012-09-07 18:51:40 -0300

    map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   I18n.locale = 'es'
 
-  def site_values
-    option = Page.find_by_key("contact")
-    @address = option.contact_address if option
+  def site_values 
     ActionMailer::Base.default_url_options[:host] = 'localhost:3000'
   end  
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,6 @@
 class Page < ActiveRecord::Base
   attr_accessible :title, :text, :contact_address, :meta_title, :meta_description, :key
+
+  geocoded_by :contact_address
+  before_validation :geocode
 end
