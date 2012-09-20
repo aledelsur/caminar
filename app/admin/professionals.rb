4614f17b4532ab7bdd7a308e4dee1252267cb2a7
Lucas Minissale <lucasminissale@gmail.com>
2012-09-20 20:45:00 -0300

    d

--- app/admin/professionals.rb
+++ app/admin/professionals.rb
@@ -5,10 +5,9 @@ ActiveAdmin.register Professional do
 
   menu :label => "Profesionales"
   index do
-    column :name
-	column :photo do |photo|
-	  image_tag(photo.photo, :class=>"home_photo_img")
-	end
+  	column :photo do |photo|
+  	  image_tag(photo.photo, :class=>"home_photo_img")
+  	end
     column :title
     column :text
     default_actions
@@ -16,8 +15,7 @@ ActiveAdmin.register Professional do
 
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Nuevo Profesional" do
-      f.input :name, :label => "Nombre y profesion (Ej: Dr. Biga, Oscar - Cardiologo)"
-      f.input :photo, :hint => f.template.image_tag(f.object.photo.url(:thumb)), :label => "Foto"
+      f.input :photo, :label => "Foto"
       f.input :title, :label => "Titulo"
       f.input :text, :label => "Texto", :as => :rich, :config => { :width => '75%', :height => '200px' }
      end
